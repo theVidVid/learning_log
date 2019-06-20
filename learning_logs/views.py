20831005f785ea7c48cd2d638f855b2b4d05c786
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-20 10:51:18 -0700

    Created new topic view method for learning_logs app.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from django.http import HttpResponseRedirect
+
+from django.urls import reverse
+
 from .models import Topic
+
+from .forms import TopicForm
 
 
 # Create your views here.
@@ -22,3 +28,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form. (GET request)
+        form = TopicForm()
+    else:
+        # POST data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
