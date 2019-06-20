cd78819ddc03a4d0f5c712cc9fad525f34825090
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-20 10:03:38 -0700

    Added detail page for a single topic to url patterns.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
 
     # Show all topics
     path('topics/', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
