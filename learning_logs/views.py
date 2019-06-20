a01cd5d3ad7a1d9f98ececd082517a8fea04eb1b
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-20 13:45:43 -0700

    Added edit_entry/ url pattern for editing existing entry, added edit_entry view method views.py.

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 
 from .forms import TopicForm, EntryForm
 
@@ -65,3 +65,23 @@ def new_entry(request, topic_id):
 
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # Initial request; pre-fill form with the current entry. (GET)
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                                args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
