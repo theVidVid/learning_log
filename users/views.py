02328840b7d9e4281fb63de829dc4d9404f4fc64
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-20 20:54:52 -0700

    Updated django.urls import statement.

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponseRedirect
 
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 from django.contrib.auth import login, authenticate
 
