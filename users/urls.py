342dee347c6b3ca84c78914a00500d2ed122a563
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-20 19:51:54 -0700

    Added a logout url pattern to users.urls, added a logout redirect url of '/' to project settings.

--- users/urls.py
+++ users/urls.py
@@ -9,8 +9,11 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    # Login page.
+    # Login page
     path('login/',
          auth_views.LoginView.as_view(template_name='users/login.html'),
          name='login'),
+
+    # Logout page
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
