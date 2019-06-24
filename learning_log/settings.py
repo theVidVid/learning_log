014e6fe240de0e7c33b462845b925bbc593f9e7f
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-23 20:05:34 -0700

    Added django_heroku import statement and Heroku specific settings in project settings file.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +132,6 @@ STATIC_URL = '/static/'
 # My settings
 LOGIN_URL = '/users/login/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Heroku settings
+django_heroku.settings(locals())
