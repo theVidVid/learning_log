21166015564d758c0662b230f02fec0cf6f7d30c
Ian Vidaurre <ianjvidaurre@gmail.com>
2019-06-26 11:08:52 -0700

    Heroku hell, oh please master... please..

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -140,10 +142,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # My settings
 LOGIN_URL = '/users/login/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Heroku settings
+django_heroku.settings(locals())
